09ecdd13d10b2d38642b0dca70892e02f8abaf53
iQLO <akaqlonist@gmail.com>
2022-04-18 04:23:50 -0500

    solution1: renderItem, renderListItem

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,65 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+type Person = {
+	name: string;
+	age: number;
+	children?: Person[];
+};
+interface DataListProps {
+	lists: Person[];
+}
+
+const DataList: React.FC<DataListProps> = ({ lists }) => {
+	const renderItem = (item: Person) => {
+		return (
+			<li>
+				{`name: ${item.name} age: ${item.age}`}
+				{item.children && renderListItems(item.children)}
+			</li>
+		);
+	};
+
+	const renderListItems = (lists: Person[]) => {
+		return (
+			<ul>
+				{lists.map((listItem, index) => {
+					return renderItem(listItem);
+				})}
+			</ul>
+		);
+	};
+	return <>{renderListItems(lists)}</>;
+};
+
+const data = [
+	{
+		name: 'Daniel',
+		age: 40,
+		children: [
+			{
+				name: 'Hunter',
+				age: 21,
+				children: [
+					{ name: 'Becky', age: 2 },
+					{ name: 'Sarah', age: 2, children: [{ name: 'Chidren', age: 13 }] },
+					{ name: 'Wendy', age: 2 },
+				],
+			},
+		],
+	},
+
+	{ name: 'John', age: 24 },
+
+	{
+		name: 'Jen',
+		age: 31,
+		children: [{ name: 'Baliey', age: 5 }],
+	},
+];
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return <DataList lists={data} />;
 }
 
 export default App;
